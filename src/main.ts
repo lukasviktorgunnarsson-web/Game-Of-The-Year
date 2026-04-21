57f13b664c3134d189359b34f907f405c553ae9d
lukasviktorgunnarsson-web <lukasviktorgunnarsson@gmail.com>
2026-04-21 10:06:02 +0200

    added functions to make start page and instructionpage work

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import startpage from "./components/startpage" 
+import startpage from "./components/startpage.ts" // FRÅGA FREDRIK VARFÖR DEN ÄR SUR
 import createPlayer from "./components/post-players"
+
+
 
 
 createPlayer();
