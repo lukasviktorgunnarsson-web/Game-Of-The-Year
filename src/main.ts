ddbe3e4bb350884369d83c4b0ace1e405bf9fa3a
lukasviktorgunnarsson-web <lukasviktorgunnarsson@gmail.com>
2026-04-24 11:54:18 +0200

    fixed all issues

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,4 @@
-import startpage from "./components/startpage.ts" // FRÅGA FREDRIK VARFÖR DEN ÄR SUR
 import createPlayer from "./components/post-players"
-
-
-
-
-import gamepage from "./components/gamepage.ts";
 
 
 
