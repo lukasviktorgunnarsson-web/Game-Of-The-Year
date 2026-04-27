ff1d9be57fdb6df2fe54c87c3fe2fc8bc004a74e
Diana <test@supersimple>
2026-04-27 09:12:37 +0200

    round1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import startpage from "./components/startpage.ts" // FRÅGA FREDRIK VARFÖR DEN ÄR SUR
-import createPlayer from "./components/post-players"
+import createPlayer from "./components/post-players.ts"
 
 import gamepage from "./components/gamepage.ts";
-
-
 
 
 createPlayer();
